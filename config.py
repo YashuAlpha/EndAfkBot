5e8f484725049c54c9462126b33efed392fe41d6
YashuAlpha <103842530+YashuAlpha@users.noreply.github.com>
2022-06-06 12:13:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = 14151343
 
 API_HASH = "9330f17086496c4580bdc8f8b24ec364"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "5347226815:AAGxEZYxNpUyTw4yAdjqmcXUaHft5gfzkrY"
 
 MONGO_DB_URI = "mongodb+srv://keshavalpha:keshavalpha@cluster0.htiys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
