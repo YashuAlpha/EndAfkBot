433747d2ae5e77705ff57fef06bea294954dddc9
YashuAlpha <103842530+YashuAlpha@users.noreply.github.com>
2022-06-06 12:15:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,6 @@ BOT_TOKEN = "5347226815:AAGxEZYxNpUyTw4yAdjqmcXUaHft5gfzkrY"
 MONGO_DB_URI = "mongodb+srv://keshavalpha:keshavalpha@cluster0.htiys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 SUDO_USER = 1927705508
+
+ALPHA_PASSWORD = 
  
